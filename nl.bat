9ae830d18db2afb946eff27c409516d6912d7ae5
sola <sola.msrluna.0812@gmail.com>
2020-04-03 20:33:09 +0900

    Create nl.bat

---
+++ nl.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Nginx Launcher -NL-
+
+cd /d %~dp0
+
+SET SERVE_DIR=C:\your\project\directory
+
+REM ---------------------------------------------------
+
+echo **** Nginx Launcher - N.L - ****
+
+cd %SERVE_DIR%
+
+start .\nginx.exe
+
+cmd /k cd %SERVE_DIR%
